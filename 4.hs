e3f93c7b5a219b3a527a8b74e4098cbfaa7f3f7d
Tinco Andringa <mail@tinco.nl>
2012-07-10 01:32:55 +0200

    still get 888888

--- 4.hs
+++ 4.hs
@@ -1,3 +1,4 @@
+import Data.List
 {- A palindromic number reads the same both ways. 
  - The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
  - Find the largest palindrome made from the product of two 3-digit numbers.
@@ -9,10 +10,16 @@ main = putStrLn $ show answer
 palindromes = filter isPalindrome productsOf3Digits
 
 productsOf3Digits = productsOf3Digits' 999 999
-productsOf3Digits' a = (sort [i * b | i <- [999,998..a], j <- [999,998..a]]) ++ rest
-  where
-    rest | a == 100 = []
-         | otherwise = productsOf3Digits' (a-1)
+productsOf3Digits' a b | a == b = a * b : productsOf3Digits' 999 (b-1) 
+                       | otherwise = a * b : productsOf3Digits' (a-1) b
+{-
+99 99
+99 98
+98 98
+99 97
+98 97
+97 97
+ -}
 
 isPalindrome n = chars == reverse chars
   where
