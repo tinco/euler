c01a1ece7810bb8e31d4f0cceb2a6e372c5a2cfb
Tinco Andringa <mail@tinco.nl>
2012-07-10 12:33:28 +0200

    gave up, easy way out..

--- 4.hs
+++ 4.hs
@@ -5,21 +5,14 @@ import Data.List
  - -}
 main = putStrLn $ show answer
   where
-    answer = head palindromes
+    answer = maximum palindromes
 
 palindromes = filter isPalindrome productsOf3Digits
 
 productsOf3Digits = productsOf3Digits' 999 999
+productsOf3Digits' 900 900 = 900 * 900 : []
 productsOf3Digits' a b | a == b = a * b : productsOf3Digits' 999 (b-1) 
                        | otherwise = a * b : productsOf3Digits' (a-1) b
-{-
-99 99
-99 98
-98 98
-99 97
-98 97
-97 97
- -}
 
 isPalindrome n = chars == reverse chars
   where
